a6d2fce96e145d5bc882856eeec7f86ad6ff8365
merunno <merunno92@gmail.com>
2021-09-20 14:14:18 +0900

    :sparkles: Slash Commmandの追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Discord from "discord.js";
 import dotenv from "dotenv";
+import * as fs from "fs";
 dotenv.config();
 
 const client = new Discord.Client({
@@ -15,4 +16,22 @@ client.login(TOKEN);
 
 client.once("ready", () => {
   console.log(`Ready: ${client.user?.username}が準備完了しました。`);
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isCommand()) return;
+  if (interaction.commandName === "help") {
+    let help = fs.readFileSync("./src/message/help.txt", "utf8");
+    let lines = help.toString().split("¥n");
+    for (let line of lines) {
+      interaction.reply({
+        content: line,
+        ephemeral: false,
+      });
+    }
+  } else if (interaction.commandName === "ping") {
+    interaction.reply({
+      content: `<a:gif_ichiyo:709701170763137054>  ${client.ws.ping}ms <:isozaki_kirito:836249519632023623> `,
+    });
+  }
 });
