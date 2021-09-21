edbbea3dbe2aebb7a26b548a482d58ef8af6f7f8
merunno <merunno92@gmail.com>
2021-09-21 13:44:07 +0900

    :wrench: チャンネルオプションの追加

--- src/register_command/command/debug.js
+++ src/register_command/command/debug.js
@@ -9,5 +9,13 @@ module.exports = {
         .setName("id")
         .setDescription("Specify the message ID to debug.")
         .setRequired(true)
+    )
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription(
+          "Specify the channel on which the message to be debugged was sent."
+        )
+        .setRequired(true)
     ),
 };
