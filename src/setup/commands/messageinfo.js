7d51bd924f0ce24d278876ddb333a5379d5ac3b0
merunno <merunno92@gmail.com>
2021-11-14 20:58:58 +0900

    [Fix] 説明欄の文字数制限の修正

--- src/setup/commands/messageinfo.js
+++ src/setup/commands/messageinfo.js
@@ -4,5 +4,5 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("message-info")
         .setDescription("Check the information in the message. / メッセージの情報を確認します。")
-        .addNumberOption((id) => id.setName('message_id').setDescription("Enter the ID of the message for which you want to check information. (Enable Developer Mode) / 情報を確認するメッセージのIDを入力してください。(開発者モードを有効)").setRequired(true))
+        .addNumberOption((id) => id.setName('message_id').setDescription("情報を確認するメッセージのIDを入力してください。(開発者モードを有効)").setRequired(true))
 };
