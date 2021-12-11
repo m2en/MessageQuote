7c71fb99536d4d40b50abf704952e187022eb09b
merunno <merunno92@gmail.com>
2021-12-12 00:54:36 +0900

    別サーバーのメッセージを引用できないようにする

--- src/run/_quote.ts
+++ src/run/_quote.ts
@@ -11,68 +11,75 @@ export function _quote(client: Client) {
     /**
      * https://(ptb.|canary.)?discord.com/channels/サーバーID/チャンネルID/メッセージID
      */
-    const regex = /https:\/\/discord.com\/channels\/\d+\/(\d+)\/(\d+)/
+    const regex = /https:\/\/discord.com\/channels\/(\d+)\/(\d+)\/(\d+)/
     const str = msg.content
     const match = str.match(regex)
     if (match === null) return
-    const [, channelID, messageID] = match
+    const [, serverID, channelID, messageID] = match
     const quoteChannel = client.channels.cache.get(channelID)
+    const quoteServerID = msg.guild?.id
 
     const errorEmbed = new MessageEmbed().setTitle('エラー').setColor('RED')
-    if (quoteChannel == null) {
+    if (serverID === quoteServerID) {
+      if (quoteChannel == null) {
+        console.error(
+          'Error: チャンネルが見つからなかったため、引用をスキップしました。'
+        )
+        return
+      }
+      if (!quoteChannel.isText()) {
+        console.error(
+          'Error: テキストチャンネルではなかったため、引用をスキップしました。'
+        )
+        return
+      }
+
+      const quoteMessage = await quoteChannel.messages.fetch(messageID)
+
+      if (quoteMessage == null) {
+        console.error(
+          'Error: メッセージが見つからなかったため、引用をスキップしました。'
+        )
+
+        return
+      }
+      if (quoteMessage.system) {
+        await msg.reply({
+          embeds: [
+            errorEmbed.setDescription('システムメッセージは引用できません。')
+          ]
+        })
+        console.error(
+          'Error: システムメッセージだったため、引用をスキップしました。'
+        )
+
+        return
+      }
+
+      const quoteEmbed = new MessageEmbed()
+        .setDescription(quoteMessage.content)
+        .setColor('#FFC9E9')
+        .setAuthor(
+          quoteMessage.author.username,
+          `${quoteMessage.author.avatarURL()}`
+        )
+        .setTimestamp(quoteMessage.createdAt)
+      if (quoteMessage.attachments.size) {
+        const [file] = quoteMessage.attachments.map(
+          (attachment) => attachment.url
+        )
+        quoteEmbed.setImage(file)
+      }
+      msg
+        .reply({
+          embeds: [quoteEmbed]
+        })
+        .catch(console.error)
+      console.log('Quote: ' + msg.author.username + 'が引用を使用.')
+    } else {
       console.error(
-        'Error: チャンネルが見つからなかったため、引用をスキップしました。'
+        'Error: 別サーバー同士の引用だったため、引用をキャンセルしました。'
       )
-      return
     }
-    if (!quoteChannel.isText()) {
-      console.error(
-        'Error: テキストチャンネルではなかったため、引用をスキップしました。'
-      )
-      return
-    }
-
-    const quoteMessage = await quoteChannel.messages.fetch(messageID)
-
-    if (quoteMessage == null) {
-      console.error(
-        'Error: メッセージが見つからなかったため、引用をスキップしました。'
-      )
-
-      return
-    }
-    if (quoteMessage.system) {
-      await msg.reply({
-        embeds: [
-          errorEmbed.setDescription('システムメッセージは引用できません。')
-        ]
-      })
-      console.error(
-        'Error: システムメッセージだったため、引用をスキップしました。'
-      )
-
-      return
-    }
-
-    const quoteEmbed = new MessageEmbed()
-      .setDescription(quoteMessage.content)
-      .setColor('#FFC9E9')
-      .setAuthor(
-        quoteMessage.author.username,
-        `${quoteMessage.author.avatarURL()}`
-      )
-      .setTimestamp(quoteMessage.createdAt)
-    if (quoteMessage.attachments.size) {
-      const [file] = quoteMessage.attachments.map(
-        (attachment) => attachment.url
-      )
-      quoteEmbed.setImage(file)
-    }
-    msg
-      .reply({
-        embeds: [quoteEmbed]
-      })
-      .catch(console.error)
-    console.log('Quote:' + quoteMessage.author.username + 'が引用を使用.')
   })
 }
