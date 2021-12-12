7cb3ecca4f520d572aee355cdbe31e78b5ba8a43
merunno <merunno92@gmail.com>
2021-12-12 13:57:13 +0900

    早期returnの追加

--- src/command/help.ts
+++ src/command/help.ts
@@ -2,31 +2,30 @@ import { Client, MessageEmbed, version } from 'discord.js'
 
 export function _help(client: Client) {
   client.on('interactionCreate', (i) => {
-    if (i.isCommand()) {
-      if (i.commandName === 'help') {
-        const embed = new MessageEmbed()
-          .setTitle('ヘルプ')
-          .setColor('#FFC9E9')
-          .setAuthor('MessageQuote', `${client.user?.avatarURL()}`)
-          .addField(
-            'メッセージ引用',
-            'メッセージリンクが送られるとそのメッセージの内容を読み込み送信します。MessageQuoteが参加しているサーバーのメッセージは全て取得できます。'
-          )
-          .addField(
-            'メッセージ引用スキップ',
-            'メッセージの最初に`;`をつけるとメッセージ引用がキャンセルされます。'
-          )
-          .addField(
-            '開発者',
-            '[merunno](https://github.com/merunno)・[mirror-kt](https://github.com/mirror-kt)・[isso0424](https://github.com/isso0424)・[loxygenK](https://github.com/loxygenK)\n**開発にご協力くださった3人、ありがとう。**'
-          )
-          .addField('実行しているdiscord.jsバージョン', version)
-          .addField(
-            'リポジトリ',
-            '[approvers/MessageQuote](https://github.com/approvers/MessageQuote)'
-          )
-        i.reply({ embeds: [embed], ephemeral: true }).catch(console.error)
-      }
-    }
+    if (!i.isCommand()) return
+    if (i.commandName !== 'help') return
+
+    const embed = new MessageEmbed()
+      .setTitle('ヘルプ')
+      .setColor('#FFC9E9')
+      .setAuthor('MessageQuote', `${client.user?.avatarURL()}`)
+      .addField(
+        'メッセージ引用',
+        'メッセージリンクが送られるとそのメッセージの内容を読み込み送信します。MessageQuoteが参加しているサーバーのメッセージは全て取得できますが、引用はされません。'
+      )
+      .addField(
+        'メッセージ引用スキップ',
+        'メッセージの最初に`;`をつけるとメッセージ引用がキャンセルされます。'
+      )
+      .addField(
+        '開発者',
+        '[merunno](https://github.com/merunno)・[mirror-kt](https://github.com/mirror-kt)・[isso0424](https://github.com/isso0424)・[loxygenK](https://github.com/loxygenK)\n**開発にご協力くださった3人、ありがとう。**'
+      )
+      .addField('実行しているdiscord.jsバージョン', version)
+      .addField(
+        'リポジトリ',
+        '[approvers/MessageQuote](https://github.com/approvers/MessageQuote)'
+      )
+    i.reply({ embeds: [embed], ephemeral: true }).catch(console.error)
   })
 }
