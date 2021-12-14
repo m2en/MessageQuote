404e7e7d1d20ac6efb1198bcc0c9da5e1df73add
merunno <merunno92@gmail.com>
2021-12-14 19:55:29 +0900

    [Add] デバック用のギルドコマンド登録ファイルの追加

---
+++ src/setup/debug.ts
@@ -0,0 +1,38 @@
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+import dotenv from 'dotenv'
+import { readdirSync } from 'fs'
+
+dotenv.config()
+
+const TOKEN = process.env.TOKEN
+const CLIENT = process.env.clientID
+const GUILD = process.env.guientID
+if (TOKEN === undefined || CLIENT === undefined || GUILD === undefined) {
+  throw new Error('Environment variable specification error.')
+}
+
+const rest = new REST({ version: '9' }).setToken(TOKEN)
+
+const commands = []
+const commandFiles = readdirSync('src/setup/commands').filter((file) =>
+  file.endsWith('.ts')
+)
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  commands.push(command.data.toJSON())
+}
+
+;(async () => {
+  try {
+    console.log('Application: Slash (/) Command - Debug Deploy Start.....')
+
+    await rest.put(Routes.applicationGuildCommands(CLIENT, GUILD), {
+      body: commands
+    })
+
+    console.log('Application: Slash (/) Command - Debug Deploy End.')
+  } catch (e) {
+    console.error(e)
+  }
+})()
