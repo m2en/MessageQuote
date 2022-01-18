2ff5c4a5fd9ebd61c4c8b6fad88a28171022ae35
merunno <merunno92@gmail.com>
2022-01-18 23:37:26 +0900

    remove: Slash / commandの廃止

--- src/setup/commands/help.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
-
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription(
-      'ヘルプを表示します。 / *help & メンションでも表示できます。'
-    )
-}
