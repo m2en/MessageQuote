17e3aee8eda84739c88915aebb233717267a8bc4
merunno <merunno92@gmail.com>
2022-01-21 17:43:14 +0900

    add: エラーをステータスチャンネルに送信する機能

--- src/run/server/index.ts
+++ src/run/server/index.ts
@@ -1,4 +1,11 @@
-import { AnyChannel, Client, version } from 'discord.js';
+import {
+  AnyChannel,
+  Client,
+  MessageActionRow,
+  MessageButton,
+  MessageEmbed,
+  version
+} from 'discord.js';
 import dotenv from 'dotenv';
 import { _help } from '../command/help';
 import { _quote } from '../_quote';
@@ -54,4 +61,49 @@ client.once('ready', async () => {
     console.error(e);
     client.destroy();
   }
+});
+
+client.on('error', async (e) => {
+  const errorName = e.name;
+  const errorMessage = e.message;
+  if (!errorName || !errorMessage) return;
+
+  const channel: AnyChannel | null = await client.channels.fetch(`${status}`);
+  if (!channel || !channel.isText()) return;
+
+  const errorEmbed = new MessageEmbed()
+    .setAuthor({ name: 'エラーが発生しました。' })
+    .setTitle(errorName)
+    .setDescription(errorMessage)
+    .addField(
+      '対処方法 / 導入Version',
+      '・<@586824421470109716> がいる場合はこのEmbedにある情報を提供してください。\n・居ない場合はTwitterかGitHubのIssueから報告してください。'
+    )
+    .addField(
+      '対処方法 / ご自身でホストしている場合',
+      'GitHubのIssueから報告してください。GitHubが使用できない場合はTwitter等で報告してください。\n自分で修正も可能ですが、おすすめしません。'
+    );
+  const twitterButton = new MessageButton()
+    .setLabel('Twitter')
+    .setStyle('LINK')
+    .setURL('https://twitter.com/2I92me_1');
+  const githubButton = new MessageButton()
+    .setLabel('GitHub / New issue')
+    .setStyle('LINK')
+    .setURL('https://github.com/approvers/MessageQuote/issues/new');
+  const discordjsButton = new MessageButton()
+    .setLabel('discord.js / errors')
+    .setStyle('LINK')
+    .setURL('https://discordjs.guide/popular-topics/errors.html');
+
+  await channel.send({
+    embeds: [errorEmbed],
+    components: [
+      new MessageActionRow().addComponents([
+        twitterButton,
+        githubButton,
+        discordjsButton
+      ])
+    ]
+  });
 });
