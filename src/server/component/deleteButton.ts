2ef7927c997bb1536a8ab805393a828d09104684
meru <piki.0147@gmail.com>
2022-03-02 19:38:09 +0900

    引用の削除を改良 (#92)
    * change: 引用操作をSelectMenuから実行できるように
    
    * Update src/server/component/deleteButton.ts
    
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>
    
    * Update src/server/service/quoteDelete.ts
    
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>
    
    * remove: テストコードの削除
    
    * Update src/server/service/quoteEvent.ts
    
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>
    
    Co-authored-by: Mikuroさいな <ryosukadnak@gmail.com>

---
+++ src/server/component/deleteButton.ts
@@ -0,0 +1,16 @@
+import { MessageActionRow, MessageSelectMenu } from 'discord.js';
+export const DELETE_BUTTON_ID = 'quoteDelete';
+export const DELETE_VALUE = 'delete_quote';
+
+export const deleteButton = new MessageActionRow().setComponents([
+  new MessageSelectMenu()
+    .setCustomId(DELETE_BUTTON_ID)
+    .setPlaceholder('引用に関する操作はこちら')
+    .addOptions([
+      {
+        label: '引用を削除',
+        description: '引用を削除します。',
+        value: DELETE_VALUE
+      }
+    ])
+]);
--- src/server/service/quoteDelete.ts
+++ src/server/service/quoteDelete.ts
@@ -1,20 +1,36 @@
-import { Client } from 'discord.js';
-export const DELETE_BUTTON_ID = 'quoteDelete';
+import {
+  CacheType,
+  Client,
+  Interaction,
+  SelectMenuInteraction
+} from 'discord.js';
+import { DELETE_BUTTON_ID, DELETE_VALUE } from '../component/deleteButton';
+
+async function deleteQuoteSystem(
+  interaction: Interaction<CacheType> | SelectMenuInteraction<CacheType>
+) {
+  if (!interaction.guild || !interaction.isSelectMenu()) return;
+  if (interaction.customId !== DELETE_BUTTON_ID) return;
+  if (!interaction.values.find((name) => name.match(DELETE_VALUE))) return;
+
+  const { channel } = interaction;
+  const quote = await channel?.messages.fetch(interaction.message.id);
+  if (!quote) return;
+
+  await quote.delete();
+  await interaction.reply({
+    content: '引用を削除しました。',
+    ephemeral: true
+  });
+  console.log(`${interaction.user.username} has removed the quote.`);
+}
 
 export function quoteDelete(client: Client<boolean>) {
   client.on('interactionCreate', async (interaction) => {
-    if (!interaction.guild || !interaction.isButton()) return;
-    if (interaction.customId !== DELETE_BUTTON_ID) return;
-
-    const channel = interaction.channel;
-    const quote = await channel?.messages.fetch(interaction.message.id);
-    if (!quote) return;
-
-    await quote.delete();
-    await interaction.reply({
-      content: '引用を削除しました。',
-      ephemeral: true
-    });
-    console.log(`${interaction.user.username} has removed the quote.`);
+    try {
+      await deleteQuoteSystem(interaction);
+    } catch (e) {
+      console.error(e);
+    }
   });
 }
