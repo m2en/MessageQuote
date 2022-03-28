36925d19de8139f3c2ae6d1ef3cf5fa058a7de1d
merunno <merunno92@gmail.com>
2022-03-28 11:29:05 +0900

    feat: add help command

---
+++ src/server/command/pingCommand.ts
@@ -0,0 +1,32 @@
+import { Client, Interaction, MessageEmbed } from 'discord.js';
+
+function createPing(client: Client, interaction: Interaction) {
+  const pingReady = new MessageEmbed()
+    .setTitle('MessageQuote Ping:')
+    .setDescription('Pinging... Please wait...')
+    .setColor('YELLOW');
+  const pingEmbed = new MessageEmbed()
+    .setTitle('MessageQuote Ping:')
+    .setDescription('🏓 Pong!')
+    .addField('Latency(WebSocket):', `${client.ws.ping}ms`)
+    .addField('Latency(API):', `${Date.now() - interaction.createdTimestamp}ms`)
+    .setColor('YELLOW');
+
+  return { pingReady, pingEmbed };
+}
+
+export function pingCommand(client: Client) {
+  client.on('interactionCreate', async (interaction) => {
+    try {
+      if (!interaction.isCommand() || !interaction.guild) return;
+      if (interaction.commandName !== 'help') return;
+
+      const { pingReady, pingEmbed } = createPing(client, interaction);
+
+      await interaction.reply({ embeds: [pingReady] });
+      await interaction.editReply({ embeds: [pingEmbed] });
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
