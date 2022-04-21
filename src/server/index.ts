50e0f01b0b686a9058a234d48e2de5103d2d226a
merunno <merunno92@gmail.com>
2022-04-21 20:54:49 +0900

    fix: エラーメッセージの日本語化及び起動ログの改善

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
-import { Client, Intents } from 'discord.js';
+import { Client, ClientUser, Intents, version } from 'discord.js';
 import * as dotenv from 'dotenv';
 import { debugCommand, helpCommand, pingCommand } from './command';
 import { errorEvent } from './event';
@@ -12,7 +12,9 @@ function setupEnv<K extends readonly string[]>(
 ): Record<K[number], string> {
   for (const key of keys) {
     if (process.env[key] == undefined) {
-      throw new Error(`Error: env(key) not found: ${key}`);
+      throw new Error(
+        `Error: 次の環境変数を取得することができませんでした: ${key} \n 環境変数の詳細は README を確認してください。`
+      );
     }
   }
 
@@ -20,7 +22,7 @@ function setupEnv<K extends readonly string[]>(
 }
 
 const env = setupEnv('DISCORD_TOKEN');
-const version = process.env.npm_package_version ?? '不明'; // versionのみ別の変数として取得する
+const clientVersion = process.env.npm_package_version ?? '不明'; // versionのみ別の変数として取得する
 
 // Discordクライアントのインスタンスを作成
 const intents = new Intents();
@@ -36,25 +38,40 @@ intents.add(
 const client = new Client({ intents });
 
 // ログイン時のイベント
+function createLoginLog(user: ClientUser) {
+  const loginLog =
+    '=========\n' +
+    '接続が完了し、MessageQuoteが利用可能になりました。\n\n' +
+    `接続元ユーザー: ${user.username}\n` +
+    `接続元ユーザーID: ${user.id}\n` +
+    `MessageQuoteのバージョン: v${clientVersion} (https://github.com/approvers/MessageQuote/releases/latest)\n` +
+    `discord.jsのバージョン: v${version} (https://github.com/discordjs/discord.js/releases)\n` +
+    '=========\n';
+  return console.log(loginLog);
+}
+
 client.login(env.DISCORD_TOKEN).catch(console.error);
 
 client.once('ready', () => {
-  console.log(`logging.....`);
+  console.log(`ログインしています....`);
 
   const clientUser = client.user;
   if (!clientUser) {
-    throw new Error('Error: client.user is undefined');
+    throw new Error(
+      'Error: 接続すべきクライアントユーザー(Bot)を取得することができませんでした。'
+    );
   }
 
   setInterval(() => {
-    clientUser.setActivity(`/help | v${version} | ping:${client.ws.ping}ms`, {
-      type: 'PLAYING'
-    });
+    clientUser.setActivity(
+      `/help | v${clientVersion} | ping:${client.ws.ping}ms`,
+      {
+        type: 'PLAYING'
+      }
+    );
   }, 1000 * 60 * 30);
 
-  console.log(
-    `Done! - Connect to ${clientUser.username}(${clientUser.id}) @ v${version}`
-  );
+  createLoginLog(clientUser);
 });
 
 // Event/Command の読み込み
