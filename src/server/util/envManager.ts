ce71ae03af30e91806cd3e74392415bf5f5cfe97
merunno <merunno92@gmail.com>
2022-04-28 15:52:19 +0900

    fix: タイポの達人

--- src/server/util/envManager.ts
+++ src/server/util/envManager.ts
@@ -2,15 +2,15 @@ import * as dotenv from 'dotenv';
 
 export function extractEnv<K extends readonly string[]>(
   keys: K,
-  defaluts?: Readonly<Partial<Record<K[number], string>>>
+  defaults?: Readonly<Partial<Record<K[number], string>>>
 ): Record<K[number], string> {
   const env: Partial<Record<K[number], string>> = {};
   for (let i = 0; i < keys.length; i++) {
     const key: K[number] = keys[i];
     if (process.env[key] !== undefined && process.env[key] !== '') {
       env[key] = process.env[key];
-    } else if (defaluts && defaluts[key] !== undefined) {
-      env[key] = defaluts[key];
+    } else if (defaults && defaults[key] !== undefined) {
+      env[key] = defaults[key];
     } else {
       throw new Error(`${key} is not defined`);
     }
